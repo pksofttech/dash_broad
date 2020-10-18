0f790c69d8910f89642cb8e7c140146f36076f1c
pksofttech <pksofttech@users.noreply.github.com>
2020-10-18 14:07:33 +0700

    make it better

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from bottle import Bottle, run, template, debug, static_file
+from bottle import get, post, request, response
+
+from bottle.ext.websocket import GeventWebSocketServer
+from bottle.ext.websocket import websocket
+
+import os, sys, socket
+
+
+app = Bottle(__name__)
+debug(False)
+
+
+
+@app.route('/')
+def index():
+    data = {"developer_name":"Ahmedur Rahman Shovon",
+            "developer_organization":"Datamate Web Solutions"}
+    return (data)
+
+
+
+# ! ------------------------------------------------------------------------------------------------------------------------ #
+
+import os
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
