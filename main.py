1613d2d8ffdb0a82b26a351e5a1ecf3c7ff6762d
pksofttech <pksofttech@users.noreply.github.com>
2020-10-18 13:28:43 +0700

    make it better

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
@@ -5,3 +6,24 @@ def hello():
     return "Hello World!"
 if __name__ == "__main__":
     app.run()
+"""
+
+from bottle import Bottle, run, template, debug, static_file
+from bottle import get, post, request, response
+
+
+service_port    = 5000;
+
+app = Bottle()
+debug(False)
+
+
+@app.route('/')
+def index():
+    return ("OK")
+
+
+
+# ! ------------------------------------------------------------------------------------------------------------------------ #
+run(app, port = service_port);
+
