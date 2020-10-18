304d5a096067adfdd166246392f904551d2e23e7
pksofttech <pksofttech@users.noreply.github.com>
2020-10-18 15:22:35 +0700

    make it better

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-# encoding: utf-8
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('43398846c9d56769235d6876bb879bba') #Your Channel Access Token
-handler = WebhookHandler('1653361761') #Your Channel Secret
+line_bot_api = LineBotApi('43398846c9d56769235d6876bb879bba')
+handler = WebhookHandler('1653361761')
 
-@app.route("/callback", methods=['POST'])
-def callback():
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -32,16 +28,14 @@ def callback():
         abort(400)
 
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    text = event.message.text #message from user
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=text)) #reply the same message from user
     
 
-import os
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run()
