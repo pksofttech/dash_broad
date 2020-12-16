7401c8621246e1fd32010df727076de2597dcefb
pksofttech <60532789+pksofttech@users.noreply.github.com>
2020-12-16 20:51:46 +0700

    make it better

--- app.py
+++ app.py
@@ -2,11 +2,21 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
-
+from pprint import pprint 
+import json 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('43398846c9d56769235d6876bb879bba')
-handler = WebhookHandler('1653361761')
+# ? หมายเหตุ: LineBotApi(ใส่ Channel Access Token ของตัวเองนะ) และ WebhookHandler(ใส่ Channel Secret ของตัวเองนะ) ดูข้อมูลได้ที่หน้า LINE Developer นะ
+
+line_bot_api = LineBotApi('ljuJWWhDu8nCahD0zsoqtPWV9GpZSKX45HVqNGmaZ47Pr/wjDWAFTdXmyd6nB+4bqCnXt+oJTeXJJv1G+FcigupmkMnmOKN4xYbLHCrbCdTr2UbECunPIBqafSQrUuPiqaVJW+uwYFAMHiiA8sIOVAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('1cfcfb0f93bcf292a4c46579fd087e52')
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="ทดสอบข้อความตอบกลับ .. {}".format(event.message.text)))
+
 
 @app.route("/")
 def hello():
@@ -19,25 +29,22 @@ def webhook():
 
     # get request body as text
     body = request.get_data(as_text=True)
+    res = json.loads(body)
     app.logger.info("Request body: " + body)
-
+    print(signature)
+    pprint(res)
     # handle webhook body
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError:
+    except InvalidSignatureError as e:
+        print(e);
         abort(400)
 
     return 'OK'
     
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
 import os
 if __name__ == "__main__":
     print(os.name);
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    #app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run(host='0.0.0.0',port=8000)
--- main.py
+++ main.py
@@ -1,75 +1,31 @@
 
-from sys import intern
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run()
+    
+"""
+
 from bottle import Bottle, run, template, debug, static_file
 from bottle import get, post, request, response
 
-#from bottle.ext.websocket import GeventWebSocketServer
-#from bottle.ext.websocket import websocket
 
-import os, sys, socket
-try:
-    from urllib import unquote
-except ImportError:
-    from urllib.parse import unquote
+service_port    = 5000;
 
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
-
-#line_bot_api = LineBotApi('43398846c9d56769235d6876bb879bba')
-#handler = WebhookHandler('1653361761')
-line_bot_api = LineBotApi('43398846c9d56769235d6876bb879bba')
-
-app = Bottle(__name__)
+app = Bottle()
 debug(False)
-
-from pprint import pprint
 
 
 @app.route('/')
 def index():
-    data = {"developer_name":"Ahmedur Rahman Shovon",
-            "developer_organization":"Datamate Web Solutions"}
-    return (data)
-
-@app.post('/')
-def index():
-
-    print("-------------------------------------------------- POST INDEX ---------------------------------------------------------------");
-    try :
-        _data = unquote(request.body.read().decode('utf-8'));
-        #_data = json.load(_data);
-        pprint(_data,indent=2);
-        rey_tk = _data["events"]["replyToken"]
-        print(rey_tk);
-        line_bot_api.reply_message(str(rey_tk), TextSendMessage(text='Hello World!'))
-    except Exception as identifier:
-        print("post Error : " + str(identifier));
-    
-    print("------------------------------------------------------------------------------------------------------------------------\n");
-    
-
-    return("POST OK")
-
-@app.post('/<post>')
-def index(post):
-    print("-------------------------------------------------- POST ---------------------------------------------------------------");
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> POST " + post);
-    try :
-        _data = unquote(request.body.read().decode('utf-8'));
-        #_data = json.load(_data);
-        print(_data);
-    except Exception as identifier:
-        print("post Error : " + str(identifier));
-    
-    
-    print("------------------------------------------------------------------------------------------------------------------------\n");
+    return ("OK")
 
 
 
 # ! ------------------------------------------------------------------------------------------------------------------------ #
+run(app, port = service_port);
 
-import os
-if __name__ == "__main__":
-    print(os.name);
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+"""
